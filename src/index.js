c2af9cc606df2a0c07796e124f51101896bbc6ca
cobachie <7006340+cobachie@users.noreply.github.com>
2020-07-01 10:59:13 +0900

    add checking the value of level option

--- src/index.js
+++ src/index.js
@@ -25,6 +25,15 @@ exports.main = (argv, stdout, stderr) => {
     }
   );
 
+  const options = {
+    level: cli.flags.level,
+  };
+
+  if (options.level < 1) {
+    stderr(`Error: Invalid level, must be greater than 0.`);
+    return 1;
+  }
+
   const dir = cli.input[0] || '.';
 
   let root;
